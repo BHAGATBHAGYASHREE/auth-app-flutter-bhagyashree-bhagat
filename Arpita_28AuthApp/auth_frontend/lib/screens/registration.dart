eff727aad9245ba871ee21a4ef86b411720011da
Arpita Jadhav <142776140+arpitajadhav9@users.noreply.github.com>
2026-01-30 14:27:49 +0530

    registration.dart updated

--- Arpita_28AuthApp/auth_frontend/lib/screens/registration.dart
+++ Arpita_28AuthApp/auth_frontend/lib/screens/registration.dart
@@ -2,99 +2,84 @@ import 'package:flutter/material.dart';
 import '../services/user_service.dart';
 import '../models/user.dart';
 import '../utils/custom_alert_box.dart';
-import 'dart:async';
+
 
 class RegistrationScreen extends StatefulWidget {
-  RegistrationScreenState createState()=> RegistrationScreenState();
-
+  RegistrationScreenState createState() => RegistrationScreenState();
 }
 
-class RegistrationScreenState extends State<RegistrationScreen>{
-
+class RegistrationScreenState extends State<RegistrationScreen> {
   final nameController = TextEditingController();
   final usernameController = TextEditingController();
   final emailController = TextEditingController();
-  final passwordController = TextEditingController(); 
+  final passwordController = TextEditingController();
 
-  void handleRegister() async{
-    User user=User(
-      id:"",
-      name:nameController.text,
-      username:usernameController.text,
-      email:emailController.text,
-      password:passwordController.text, 
-    );
-    final response = await UserService.register(user);
-    print(response);
+void handleRegister() async {
+  User user = User(
+    id: "",
+    name: nameController.text,
+    username: usernameController.text,
+    email: emailController.text,
+    password: passwordController.text,
+  );
 
-    if (response['message']=='User registered successfully'){
-      CustomAlertBox.showAlert(
-        context,
-        title: 'Success',
-        message: response['message']);
-        Timer(Duration(seconds: 2), ()=>Navigator.pushNamed(context, '/login'));
-          Navigator.pushNamed(context, '/login');
-          
+  final response = await UserService.register(user);
+  print(response);
 
-    } else {
-      CustomAlertBox.showAlert(
-        context,
-        title: 'Error',
-        message: response['message'],
-      );
-    }
-
+  if (response['message'].toString().toLowerCase() == 'user registered successfully') {
+    await CustomAlertBox.showSuccess(context,'Success',response['message'],);
+    Navigator.pushNamed(context, '/login');
+  } else {
+    CustomAlertBox.showError(context,'Error',response['message'],);
   }
+}
 
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Registration '),
+        title: Text("Registration"),
       ),
       body: Column(
         children: [
           TextField(
             controller: nameController,
             decoration: InputDecoration(
-              labelText: 'Name',
-              hintText: 'Enter your name',
-              icon:Icon(Icons.person)
-            ),
+              labelText: "Name",
+              hintText: "Enter your name",
+              icon: Icon(Icons.person)
+            )
           ),
           TextField(
             controller: usernameController,
             decoration: InputDecoration(
-              labelText: 'Username',
-              hintText: 'Enter your username',
-              icon:Icon(Icons.person)
-            ),
+              labelText: "Username",
+              hintText: "Enter your username",
+              icon: Icon(Icons.person)
+            )
           ),
           TextField(
             controller: emailController,
             decoration: InputDecoration(
-              labelText: 'Email',
-              hintText: 'Enter your email',
-              icon:Icon(Icons.email)
-            ),
+              labelText: "Email",
+              hintText: "Enter your email",
+              icon: Icon(Icons.email)
+            )
           ),
           TextField(
             controller: passwordController,
             decoration: InputDecoration(
-              labelText: 'Password',
-              hintText: 'Enter your password',
-              icon:Icon(Icons.password)
-            ),
+              labelText: "Password",
+              hintText: "Enter your password",
+              icon: Icon(Icons.password)
+            )
           ),
           TextButton(
-            child:Text("Register"),
-            onPressed: (){
-              handleRegister();
-            }
+            child: Text("Register"),
+            onPressed: handleRegister,
           )
-
-        ]
-      )
+        ],
+      ),
     );
   }
 }
